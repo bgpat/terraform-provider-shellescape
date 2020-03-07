c443c23caf978f66f69d85e6807d7558de232ae1
bgpat <bgpat@users.noreply.github.com>
2020-03-07 22:19:54 +0900

    Extract as shellescape package

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+
+	"github.com/bgpat/terraform-provider-shellescape/shellescape"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
-			return Provider()
+			return shellescape.Provider()
 		},
 	})
 }
