04b3534ba3fc19cb63a4510439c47c4c704cc272
bgpat <bgpat@users.noreply.github.com>
2020-03-07 14:57:09 +0900

    Implement shellescape_quote

---
+++ datasource_shellescape_string.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"strconv"
+
+	"github.com/alessio/shellescape"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/hashcode"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceShellescapeQuote() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceShellescapeQuoteRead,
+		Schema: map[string]*schema.Schema{
+			"string": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"quoted": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceShellescapeQuoteRead(d *schema.ResourceData, m interface{}) error {
+	quoted := shellescape.Quote(d.Get("string").(string))
+	d.Set("quoted", quoted)
+	d.SetId(strconv.Itoa(hashcode.String(quoted)))
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/bgpat/terraform-provider-shellescape
+
+go 1.13
+
+require (
+	github.com/alessio/shellescape v1.2.2
+	github.com/hashicorp/go-getter v1.4.2-0.20200106182914-9813cbd4eb02 // indirect
+	github.com/hashicorp/hcl/v2 v2.3.0 // indirect
+	github.com/hashicorp/terraform-config-inspect v0.0.0-20191212124732-c6ae6269b9d7 // indirect
+	github.com/hashicorp/terraform-plugin-sdk v1.7.0
+)
